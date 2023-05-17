bcdd10c84d3aed07269217591a602fac9fef6822
Monyjenni <chansovanmonyyoeun03@gmail.com>
2023-05-17 21:23:50 +0700

    twobinding_vue.js

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<!-- view part -->
+<template>
+  <div>
+    <h1>Yoo</h1>
+    <!-- {{  }} is used to print the data -->
+    <h2>{{ data }}</h2>
+    <input type="text" v-model="data" />
+  </div>
+</template>
+
+<!-- model part__will be changed, reflect and update -->
+<script>
+export default {
+  //name should has multi wordss
+  name: "JennieProfile",
+  // return properties
+  data() {
+    return {
+      data:0,
+    };
+  },
+};
+</script>
